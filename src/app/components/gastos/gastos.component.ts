36eae818f740d630f23496ad636db1b42f67b47c
joaohdib <joaohdib@gmail.com>
2025-01-06 15:06:40 -0300

    options on the in and out page updated

--- src/app/components/gastos/gastos.component.ts
+++ src/app/components/gastos/gastos.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { GastoInfoComponent } from './components/gasto-info/gasto-info.component';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @Component({
   selector: 'app-gastos',
-  imports: [MatButtonModule, GastoInfoComponent],
+  imports: [MatButtonModule, GastoInfoComponent, MatSelectModule],
   templateUrl: './gastos.component.html',
   styleUrl: './gastos.component.css'
 })
