40d14cddff4bd33a692e7d8afba005899de75910
joaohdib <joaohdib@gmail.com>
2025-02-18 18:09:05 -0300

    custom theme alterado, tela de login feita

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { GastosComponent } from './components/gastos/gastos.component';
 import { IndexComponent } from './components/index/index.component';
 import { SignUpComponent } from './components/index/sign-up/sign-up.component';
+import { AddGastoComponent } from './components/gastos/components/add-gasto/add-gasto.component';
 import { AuthGuard } from './guards/login.guard';
 import { AuthGuard2 } from './guards/index.guard';
 
@@ -11,5 +12,6 @@ export const routes: Routes = [
     { path: '', component: IndexComponent, canActivate: [AuthGuard2]},
     { path: 'home', component: HomeComponent,  canActivate: [AuthGuard] },
     { path: 'gastos', component: GastosComponent },
+    { path: 'gastos/add', component: AddGastoComponent },
     { path: 'signup', component: SignUpComponent },
 ];
--- src/app/components/gastos/components/add-gasto/add-gasto.component.ts
+++ src/app/components/gastos/components/add-gasto/add-gasto.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, ViewEncapsulation  } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-add-gasto',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, FormsModule],
   templateUrl: './add-gasto.component.html',
   styleUrl: './add-gasto.component.css'
 })
 export class AddGastoComponent {
+  email:any;
+  password: any;
 
 }
