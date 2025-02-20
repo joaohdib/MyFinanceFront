9f994a06d25d1d475ce858bff50122f9ded105b5
joaohdib <joaohdib@gmail.com>
2025-02-19 23:31:21 -0300

    select option on add payment

--- src/app/components/gastos/components/add-gasto/add-gasto.component.ts
+++ src/app/components/gastos/components/add-gasto/add-gasto.component.ts
@@ -5,16 +5,34 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import axios from 'axios';
 
 
 @Component({
   selector: 'app-add-gasto',
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, FormsModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, FormsModule,MatSelectModule],
   templateUrl: './add-gasto.component.html',
   styleUrl: './add-gasto.component.css'
 })
 export class AddGastoComponent {
-  email:any;
-  password: any;
+  date:any;
+  value: any;
+  category: any;
+
+
+  submit() {
+    axios.post('http://localhost:3000/payment', {
+      quantity: this.value,
+      category: this.category,
+      month: new Date(this.date).getMonth() + 1,
+      year: new Date(this.date).getFullYear(),
+      type: 'expense'
+    }).then(response => {
+      console.log(response.data);
+    }).catch(error => {
+      console.error('Error:', error);
+    });
+  }
 
 }
